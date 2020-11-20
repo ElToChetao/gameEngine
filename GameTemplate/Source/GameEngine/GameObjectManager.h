468c1fcd42dae9c24f382d305a3ed896c9bad516
Ernesto <erni.uson@gmail.com>
2020-11-20 04:29:31 +0100

    Hay que colocar las cosas en el source

---
+++ GameTemplate/Source/GameEngine/GameObjectManager.h
@@ -0,0 +1,81 @@
+#pragma once
+#include "Singleton.h"
+#include "SDL.h"
+#include <Texture.h>
+#include <vector>
+
+using namespace std;
+class Vector2 {
+public:
+	int x;
+	int y;
+	Vector2() {
+		x = 0;
+		y = 0;
+	}
+	Vector2(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	Vector2& operator+=(const Vector2& other) {
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
+};
+
+
+class Transform {
+public:
+	Vector2 position;
+	int rotation;
+
+	Transform() {
+		rotation = 0;
+	}
+};
+class GameObject {
+public:
+	Transform transform;
+	bool isActive;
+	LTexture texture;
+	GameObject(string spritePath) {
+		isActive = true;
+
+		if (!texture.loadFromFile(spritePath))
+		{
+			printf("Failed to load dot texture!\n");
+		}
+	}
+	void translate(Vector2 offset) {
+		transform.position += offset;
+	}
+	void render() {
+		texture.render(transform.position.x, transform.position.y);
+	}
+	void update() {
+
+	}
+};
+
+class GameObjectManager :public Singleton<GameObjectManager>
+{
+	friend class Singleton<GameObjectManager>;
+private:
+	vector<GameObject*> gameObjects;
+	GameObjectManager() {};
+public:
+
+	void Update(void);
+
+	void AddGameObject(GameObject*);
+	vector<GameObject*> GetGameObjects() { return gameObjects; };
+};
+void GameObjectManager::Update() {
+	for (int i = 0; i < gameObjects.size(); i++) {
+		gameObjects[i]->update();
+	}
+}
+void GameObjectManager::AddGameObject(GameObject *go) {
+	gameObjects.push_back(go);
+}
