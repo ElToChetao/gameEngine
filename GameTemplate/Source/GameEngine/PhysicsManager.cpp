e4001c05945692cd3f30ede956438230d26f4d39
Ernesto <erni.uson@gmail.com>
2020-12-06 17:56:26 +0100

    gracias david por los if(isRect == true)

--- GameTemplate/Source/GameEngine/PhysicsManager.cpp
+++ GameTemplate/Source/GameEngine/PhysicsManager.cpp
@@ -5,16 +5,12 @@ PhysicsManager::PhysicsManager(){}
 void PhysicsManager::Update() {
 	vector<GameObject*> go = GameObjectManager::GetInstance().GetGameObjects();
 	for (int i = 0; i < go.size(); i++) {
-		for (int j = 0; j < go.size(); j++)
+		for (int j = i + 1; j < go.size(); j++)
 		{
-			if (i != j)
+			if (CheckCollisions(go[i], go[j]))
 			{
-				bool collision = CheckCollisions(go[i], go[j]);
-				if (collision == true)
-				{
-					go[i]->onCollision(go[j]);
-					go[j]->onCollision(go[i]);
-				}
+				go[i]->onCollision(go[j]);
+				go[j]->onCollision(go[i]);
 			}
 		}
 	}
@@ -22,25 +18,18 @@ void PhysicsManager::Update() {
 
 bool PhysicsManager::CheckCollisions(GameObject* go, GameObject* other) {
 
-	if (go->collider.isRect == true && other->collider.isRect == true)
+	if (go->collider.isRect && other->collider.isRect)
 	{
 		return CheckRectCollisions(go, other);
 	}
-	else if (go->collider.isRect == false && other->collider.isRect == false)
+	else if (!go->collider.isRect && !other->collider.isRect)
 	{
 		return CheckCircleCollisions(go, other);
 	}
 	else
 	{
 		//go must always be RectCollider
-		if (go->collider.isRect == true)
-		{
-			return CheckRectCircleCollisions(go, other);
-		}
-		else
-		{
-			return CheckRectCircleCollisions(other, go);
-		}
+		return go->collider.isRect ? CheckRectCircleCollisions(go, other) : CheckRectCircleCollisions(other, go);
 	}
 }
 
@@ -50,23 +39,15 @@ bool PhysicsManager::CheckCircleCollisions(GameObject* go, GameObject* other)
 	float dy = (go->transform.position.y - go->collider.radius) - (other->transform.position.y - other->collider.radius);
 	float distance = sqrt((dx * dx) + (dy * dy));
 
-	if (distance < go->collider.radius + other->collider.radius)
-	{
-		return true;
-	}
-	return false;
+	return distance < go->collider.radius + other->collider.radius;
 }
 
 bool PhysicsManager::CheckRectCollisions(GameObject* go, GameObject* other)
 {
-	if (go->transform.position.x < other->transform.position.x + other->transform.size.x &&
+	return go->transform.position.x < other->transform.position.x + other->transform.size.x &&
 		go->transform.position.x + other->transform.size.x > other->transform.size.x &&
 		go->transform.position.y < other->transform.position.y + other->transform.size.y &&
-		go->transform.size.y + go->transform.position.y > other->transform.position.y) {
-
-		return true;
-	}
-	return false;
+		go->transform.size.y + go->transform.position.y > other->transform.position.y;
 }
 
 //Go always rect
@@ -99,8 +80,5 @@ bool PhysicsManager::CheckRectCircleCollisions(GameObject* rect, GameObject* cir
 	float distance = sqrt((distX * distX) + (distY * distY));
 
 	// if the distance is less than the radius, collision!
-	if (distance <= circle->collider.radius) {
-		return true;
-	}
-	return false;
+	return distance <= circle->collider.radius;
 }
